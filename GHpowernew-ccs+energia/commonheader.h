97ad6dea6da45fed1cfcb87121702a83dff0f1ce
Abhishek <abhi11796acharya@gmail.com>
2016-07-05 03:37:40 +0530

    wifi connected

---
+++ GHpowernew-ccs+energia/commonheader.h
@@ -0,0 +1,85 @@
+/*
+ * commonheader.h
+ *
+ *  Created on: Jun 25, 2016
+ *      Author: Abhishek Acharya
+ */
+
+#ifndef COMMONHEADER_H_
+#define COMMONHEADER_H_
+
+// STANDARD DIRECTORIES
+#include "stdint.h"
+#include "stdio.h"
+#include "stdbool.h"
+#include "stdlib.h"
+#include "math.h"
+//#include "cstdarg"
+//#include "pin_mux_config.h"
+
+//INC DIRECTORIES
+#include "inc/hw_adc.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_uart.h"
+#include "inc/hw_common_reg.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_apps_rcm.h"
+#include "inc/hw_common_reg.h"
+#include "inc/hw_gpio.h"
+#include "inc/asmdefs.h"
+#include "inc/hw_aes.h"
+#include "inc/hw_apps_config.h"
+#include "inc/hw_camera.h"
+#include "inc/hw_des.h"
+#include "inc/hw_dthe.h"
+#include "inc/hw_flash_ctrl.h"
+#include "inc/hw_gprcm.h"
+#include "inc/hw_hib1p2.h"
+#include "inc/hw_hib3p3.h"
+#include "inc/hw_i2c.h"
+#include "inc/hw_mcasp.h"
+#include "inc/hw_mcspi.h"
+#include "inc/hw_mmchs.h"
+#include "inc/hw_nvic.h"
+#include "inc/hw_ocp_shared.h"
+#include "inc/hw_shamd5.h"
+#include "inc/hw_stack_die_ctrl.h"
+#include "inc/hw_udma.h"
+#include "inc/hw_wdt.h"
+
+//DIRVERLIB DIRECTORIES
+#include "driverlib/adc.h"
+#include "driverlib/aes.h"
+#include "driverlib/camera.h"
+#include "driverlib/cpu.h"
+#include "driverlib/crc.h"
+#include "driverlib/debug.h"
+#include "driverlib/des.h"
+#include "driverlib/flash.h"
+#include "driverlib/gpio.h"
+#include "driverlib/hwspinlock.h"
+#include "driverlib/i2c.h"
+#include "driverlib/i2s.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin.h"
+#include "driverlib/prcm.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/rom_patch.h"
+#include "driverlib/sdhost.h"
+#include "driverlib/shamd5.h"
+#include "driverlib/spi.h"
+#include "driverlib/systick.h"
+#include "driverlib/timer.h"
+#include "driverlib/uart.h"
+#include "driverlib/udma.h"
+#include "driverlib/utils.h"
+#include "driverlib/version.h"
+#include "driverlib/wdt.h"
+
+
+
+#endif /* COMMONHEADER_H_ */
